fa54d932fc31bde434007c9aa11806acd4ef2b16
Imran Namazoglu <imran.nmzv@gmail.com>
2026-06-29 14:18:21 +0300

    Rename nn.py & engine.py

--- vector_nn.py
+++ vector_nn.py
@@ -29,15 +29,15 @@ class MLP:
 
     def __call__(self, inputs):
         # the ys and zs start from the first layer
-        z = []
-        y = []
+        zs = []
+        ys = []
         activated_output = inputs
         for layer in self.layers:
             activated_output, raw_output = layer(activated_output)
-            z.append(raw_output)
-            y.append(activated_output)
+            zs.append(raw_output)
+            ys.append(activated_output)
         # returns the output of the mlp, & the zs + ys calculated during forward pass, for each layer
-        return activated_output, z, y
+        return activated_output, zs, ys
 
     
     def __repr__(self):
@@ -45,8 +45,11 @@ class MLP:
 
     def _backward(self, inputs, targets):
         # forward pass
-        z = [];     y = []
+        z = []
+        y = []
+        
         outputs, z, y = self(inputs)
+        
         z = list(reversed(z))
         y = list(reversed(y)); y.append(inputs)
 
@@ -64,13 +67,4 @@ class MLP:
             layer.bgrad = np.sum(layer.dL_dz, axis=0)
 
 if __name__ == "__main__":
-    # mlp = MLP([3, 5, 2], ["relu", "sigmoid"])
-    # x = np.array([[0.1, 0.2, 0.3], [0.4, 0.32, 0.13], [0.1, 0.212, 0.3] ,[0.11, 0.2, 0.3], [0.1, 0.2, 0.3]])
-    # output = mlp(x)
-
-
-
-
-    # print(mlp)
-    # print("-" * 40)
-    # print("Output of MLP: ", output)
+    pass
